6da7bc58270fedc999d904caa478781444fc66d9
steven-king <cujophoto@gmail.com>
2016-08-23 10:46:24 -0400

    removing old stuff

--- _classes/quiz4.js
+++
@@ -1,6 +0,0 @@
-var students = ["Joe", "Jack", "Jane", "Jill"];
-var gradeMean = [90, 68, 92, 88];
-
-//Using the data above write a JavaScript that is linked to an html page that determines who has the lowest score.
-//Write the answer in sentence form that includes both the name of the student and the score they have.
-//Write the answer to both the console and the html page using the document write or other command.
